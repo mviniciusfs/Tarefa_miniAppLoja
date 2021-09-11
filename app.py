1fb1ad5ef750e7aec158050c6a85b6ad3ec93ef8
mviniciusfs <50427044+mviniciusfs@users.noreply.github.com>
2021-09-10 21:10:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ def lista():  # put application's code here
     lista_prod = {'#1':
                       ['Arroz Sepé', 'R$20,99', 'https://www.extrabom.com.br/uploads/produtos/350x350/7859_7896009865657.jpg'],
                   '#2':
-                      ['Feijão Ana', 'R$7,99', 'https://wilso.agilecdn.com.br/4206_1.jpg'],
+                      ['Feijão Ana', 'R$7,99', 'https://produtosriodoce.com.br/wp-content/uploads/2019/11/feijao_preto_1kg_ana-1024x1024.png'],
                   '#3':
                       ['Fubá Yoki', 'R$5,99', 'https://api.tendaatacado.com.br/fotos/4853_big.jpg']
                   }
-    return render_template('index.html')
+    return render_template('index.html', lista_prod=lista_prod)
 
 if __name__ == '__main__':
     app.run()
